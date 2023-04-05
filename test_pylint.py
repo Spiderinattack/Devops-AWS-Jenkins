f688cbfdabdc3ad418831ac370ba065435a4bc1f
AWS-Jenkins <124871418+Spiderinattack@users.noreply.github.com>
2023-04-05 17:23:10 +0530

    Create test_pylint.py

---
+++ test_pylint.py
@@ -0,0 +1,10 @@
+import argparse
+import logging
+from pylint.lint import Run
+results = Run(['Programs/code1.py'], do_exit=False)
+final_score = results.linter.stats.global_note
+final_score
+
+results = Run(['Programs/code1new.py'], do_exit=False)
+final_score = results.linter.stats.global_note
+final_score
