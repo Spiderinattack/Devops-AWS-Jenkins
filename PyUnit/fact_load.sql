54040a4197706437d5fe2f5446871a2ed0b9d58a
AWS-Jenkins <124871418+Spiderinattack@users.noreply.github.com>
2023-04-10 17:28:29 +0530

    Create fact_load.sql

---
+++ PyUnit/fact_load.sql
@@ -0,0 +1,46 @@
+.mode csv
+
+CREATE temp TABLE [order_delta](
+   [OrderID]INTEGER ,
+   [CustomerID]TEXT,
+   [EmployeeID]INTEGER,
+   [OrderDate]DATETIME,
+   [RequiredDate]DATETIME,
+   [ShippedDate]DATETIME,
+   [ShipVia]INTEGER,
+   [Freight]NUMERIC DEFAULT 0,
+   [ShipName]TEXT,
+   [ShipAddress]TEXT,
+   [ShipCity]TEXT,
+   [ShipRegion]TEXT,
+   [ShipPostalCode]TEXT,
+   [ShipCountry]TEXT
+);
+
+.import order_delta.csv order_delta
+
+insert into fact_order
+select 
+o.OrderID,
+o.CustomerID,
+c.ContactName,
+c.Phone,
+o.EmployeeID,
+e.FirstName,
+e.LastName,
+o.OrderDate,
+o.RequiredDate,
+o.ShippedDate,
+o.ShipVia,
+t.TerritoryDescription,
+o.Freight,
+o.ShipName,
+o.ShipAddress,
+o.ShipCity,
+o.ShipRegion,
+o.ShipPostalCode,
+o.ShipCountry 
+from order_delta o 
+left join employees e on o.employeeid=e.employeeid
+left join customers c on o.customerid=c.customerid
+left join territories t on  o.shipvia=t.TerritoryID;
