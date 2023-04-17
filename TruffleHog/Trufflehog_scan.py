95944daf6485375664bbc12e657968a75f6b562d
AWS-Jenkins <124871418+Spiderinattack@users.noreply.github.com>
2023-04-17 13:13:42 +0530

    Update Trufflehog_scan.py

--- TruffleHog/Trufflehog_scan.py
+++ TruffleHog/Trufflehog_scan.py
@@ -1,5 +1,8 @@
-pip install truffleHog
+#to install trfflehog if not
+pip3 install truffleHog
 
-truffleHog --json --regex --entropy=False /var/lib/jenkins/workspace/Python Job
-
-#truffleHog --json -–regex /var/lib/jenkins/workspace/Python Job
+#to check trufflehog entropy
+trufflehog https://github.com/Spiderinattack/Devops-AWS-Jenkins.git
+  
+#to check trufflehog regex
+trufflehog --regex --entropy False https://github.com/Spiderinattack/Devops-AWS-Jenkins.git
