7ff7012aef4e3f189aa2df2972bfe3bced7d729f
AWS-Jenkins <124871418+Spiderinattack@users.noreply.github.com>
2023-04-17 19:20:06 +0000

    Update compare_pylint.py

--- compare_pylint.py
+++ compare_pylint.py
@@ -1,4 +1,10 @@
 import os
 
 os.system('pwd')
-os.system("sed -n 's/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p' results/pylint_result.txt")
+# os.system("sed -n 's/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p' results/pylint_result.txt")
+score = os.popen("sed -n 's/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p' results/pylint_result.txt").read()
+
+if score > 7:
+  print("score > than 7")
+else:
+  print("score < than 7")
