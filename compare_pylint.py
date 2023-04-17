af1dd82a705c976ae9e7c963ee56f41bc82a7971
AWS-Jenkins <124871418+Spiderinattack@users.noreply.github.com>
2023-04-17 19:26:15 +0000

    Update compare_pylint.py

--- compare_pylint.py
+++ compare_pylint.py
@@ -2,7 +2,8 @@ import os
 
 os.system('pwd')
 # os.system("sed -n 's/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p' results/pylint_result.txt")
-score = os.popen("sed -n 's/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p' results/pylint_result.txt").read()
+os.system('cd results')
+score = os.popen("sed -n 's/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p' pylint_result.txt").read()
 print("score is: ", score)
 print("Type of score is: ", type(score))
 
