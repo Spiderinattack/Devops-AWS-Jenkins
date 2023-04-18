f4500164256cd8a6ab9b27c2f825ef1e6f9a5b58
AWS-Jenkins <124871418+Spiderinattack@users.noreply.github.com>
2023-04-18 06:43:19 +0000

    Update compare_pylint.py

--- compare_pylint.py
+++ compare_pylint.py
@@ -8,15 +8,15 @@ os.chdir('results')
 print('Current Working Directory is: ', os.getcwd())
 # os.system('cd results')
 # score = os.popen("sed -n 's/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p' pylint_result.txt").read()
-os.chdir('/home/ec2-user/Jenkins/Dev/Jenkins/workspace/new_Job/results/')
+os.chdir('/home/ec2-user/Jenkins/Dev/Jenkins/workspace/Python_Job/results/')
 print('Current new Working Directory is: ', os.getcwd())
-# score = subprocess.call(["sed -n 's/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p' pylint_result.txt"], shell=True)
+score = subprocess.call(["sed -n 's/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p' pylint_result.txt"], shell=True)
 
 
-regex = re.compile('s/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p')
-with open("pylint_result.txt") as f:
-    for line in f:
-        score = regex.search(line)
+# regex = re.compile('s/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p')
+# with open("pylint_result.txt") as f:
+#     for line in f:
+#         score = regex.search(line)
 print("score is: ", score)
 print("Type of score is: ", type(score))
 
