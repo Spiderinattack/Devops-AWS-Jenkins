a3d9489760bb27803f4e32f70b3332b3e931c46b
AWS-Jenkins <124871418+Spiderinattack@users.noreply.github.com>
2023-04-18 14:58:23 +0530

    Update Trufflehog_scan.py

--- TruffleHog/Trufflehog_scan.py
+++ TruffleHog/Trufflehog_scan.py
@@ -2,7 +2,15 @@
 #pip3 install truffleHog
 
 #to check trufflehog entropy
-trufflehog https://github.com/Spiderinattack/Devops-AWS-Jenkins.git
+#trufflehog https://github.com/Spiderinattack/Devops-AWS-Jenkins.git
   
 #to check trufflehog regex
-trufflehog --regex --entropy False https://github.com/Spiderinattack/Devops-AWS-Jenkins.git
+#trufflehog --regex --entropy False https://github.com/Spiderinattack/Devops-AWS-Jenkins.git
+  
+ rm -f results/trufflehog_result.txt || true
+ pip3 install truffleHog
+ sudo chmod 777 /home/ec2-user/Jenkins/Dev/Jenkins/workspace/Python_Job/TruffleHog/Trufflehog_scan.py
+ trufflehog --json https://github.com/Spiderinattack/Devops-AWS-Jenkins.git >> results/trufflehog_result.txt
+ trufflehog --json --regex --entropy False https://github.com/Spiderinattack/Devops-AWS-Jenkins.git >> results/trufflehog_result.txt
+ print("TruffleHog Report generated successfully with entropy and regex checks")
+ sudo chmod 777  /home/ec2-user/Jenkins/Dev/Jenkins/workspace/Python_Job/results/trufflehog_result.txt
