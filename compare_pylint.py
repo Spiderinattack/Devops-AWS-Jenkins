ad14cbb8d14b3a81c8ccbf88bdaf41292065827f
AWS-Jenkins <124871418+Spiderinattack@users.noreply.github.com>
2023-04-18 05:29:48 +0000

    Update compare_pylint.py

--- compare_pylint.py
+++ compare_pylint.py
@@ -3,6 +3,7 @@ import os
 os.system('pwd')
 # os.system("sed -n 's/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p' results/pylint_result.txt")
 os.chdir('results')
+print('Current Working Directory is: ', os.getcwd())
 # os.system('cd results')
 score = os.popen("sed -n 's/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p' pylint_result.txt").read()
 print("score is: ", score)
