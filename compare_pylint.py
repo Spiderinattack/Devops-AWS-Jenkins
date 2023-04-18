3ff3c0527f7023efd1079a9c1ab5dc52f655f949
AWS-Jenkins <124871418+Spiderinattack@users.noreply.github.com>
2023-04-18 09:07:19 +0000

    Update compare_pylint.py

--- compare_pylint.py
+++ compare_pylint.py
@@ -11,15 +11,15 @@ os.system('pwd')
 print('Current new Working Directory is: ', os.getcwd())
 # score = os.popen("sed -n 's/^Final score is \([-0-9.]*\)\/.*/\1/p' pylint_result.txt").read()
 
-# sed_command = "sed -n 's/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p' /home/ec2-user/Jenkins/Dev/Jenkins/workspace/Python_Job/results/pylint_result.txt"
-sed_command = ["sed", "-n", "s/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p", "/home/ec2-user/Jenkins/Dev/Jenkins/workspace/Python_Job/results/pylint_result.txt"]
+sed_command = "sed -n 's/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p' /home/ec2-user/Jenkins/Dev/Jenkins/workspace/Python_Job/results/pylint_result.txt"
+# sed_command = ["sed", "-n", "s/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p", "/home/ec2-user/Jenkins/Dev/Jenkins/workspace/Python_Job/results/pylint_result.txt"]
 # score = subprocess.call(['sed -n "s/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p" pylint_result.txt'], shell=True)
 # score = subprocess.run(sed_command, shell=True, capture_output=True, text=True)
-# score = subprocess.check_output(sed_command, shell=True, text=True)
+score = subprocess.check_output(sed_command, shell=True, text=True, encoding='utf-8')
 score = subprocess.Popen(sed_command, stdout=subprocess.PIPE, text=True)
 
 # print the output
-print(score.communicate()[0])
+# print(score.communicate()[0])
 print(score)
 # score = subprocess.call(["sed -n 's/^Final score is \([-0-9.]*\)\/.*/\1/p' pylint_result.txt"], shell=True)
 
