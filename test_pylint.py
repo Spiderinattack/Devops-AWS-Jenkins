1f284e5aaeae97d5d10dc2ed6e54ce08030508a9
AWS-Jenkins <124871418+Spiderinattack@users.noreply.github.com>
2023-04-18 06:50:54 +0000

    Update test_pylint.py

--- test_pylint.py
+++ test_pylint.py
@@ -4,7 +4,8 @@ from pylint.lint import Run
 results = Run(['Programs/code1.py'], do_exit=False)
 final_score = results.linter.stats.global_note
 final_score
+print("Type of score: ", type(final_score))
 
-# results = Run(['Programs/code1new.py'], do_exit=False)
-# final_score = results.linter.stats.global_note
-# final_score
+results = Run(['Programs/code1new.py'], do_exit=False)
+final_score = results.linter.stats.global_note
+final_score
