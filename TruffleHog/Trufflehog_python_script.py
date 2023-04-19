26e43315bde02565fae2d16c7e04d286c8f766f9
AWS-Jenkins <124871418+Spiderinattack@users.noreply.github.com>
2023-04-19 22:33:04 +0530

    Update Trufflehog_python_script.py

--- TruffleHog/Trufflehog_python_script.py
+++ TruffleHog/Trufflehog_python_script.py
@@ -8,16 +8,19 @@ print(install_trufflehog)
 # Specify the repository URL and directory to clone it to
 repo_url = 'https://github.com/Spiderinattack/Devops-AWS-Jenkins.git'
 repo_dir = '/var/lib/jenkins/workspace/Trufflehog_Job'
-   
+
+output = subprocess.check_output('trufflehog3 /var/lib/jenkins/workspace/Trufflehog_Job', shell=True, text=True, encoding='utf-8')
+print("Output :", output)
+
 # subprocess.Popen() method is used to run the trufflehog3 command with the directory
 # shell=True argument indicates that the command should be executed using a shell
 # stdout=subprocess.PIPE argument redirects the output of the command to a pipe
 # stderr=subprocess.PIPE argument redirects the error of the command to a pipe
 # communicate() method waits for the command to complete and returns a tuple of (stdout, stderr) 
-output = subprocess.Popen(['trufflehog3 /var/lib/jenkins/workspace/Trufflehog_Job', '/var/lib/jenkins/workspace/Trufflehog_Job'], shell=True, stdout=subprocess.PIPE).communicate()[0]
+#output = subprocess.Popen(['trufflehog3 /var/lib/jenkins/workspace/Trufflehog_Job', '/var/lib/jenkins/workspace/Trufflehog_Job'], shell=True, stdout=subprocess.PIPE).communicate()[0]
 
 # Print the output
-print(output.decode('utf-8'))
+#print(output.decode('utf-8'))
 
 # Open the output file in write mode
 #with open('TruffleHog/results.txt', 'w') as f:
