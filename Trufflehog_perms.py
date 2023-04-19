084be973b2126f102b85ae87aa4177f317602082
AWS-Jenkins <124871418+Spiderinattack@users.noreply.github.com>
2023-04-19 17:35:56 +0530

    Create Trufflehog_perms.py

---
+++ Trufflehog_perms.py
@@ -0,0 +1,4 @@
+import os
+
+os.system('sudo chmod 777 /var/lib/jenkins/workspace/Trufflehog_Job/TruffleHog/trufflehog_result.json')
+os.system('sudo chmod 777 /var/lib/jenkins/workspace/Trufflehog_Job/TruffleHog/Trufflehog_scan.py')
