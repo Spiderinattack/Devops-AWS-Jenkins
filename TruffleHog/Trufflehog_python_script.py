0bbeb6d127822fdaba02dee436a75b1b7185ab34
AWS-Jenkins <124871418+Spiderinattack@users.noreply.github.com>
2023-04-19 22:28:42 +0530

    Update Trufflehog_python_script.py

--- TruffleHog/Trufflehog_python_script.py
+++ TruffleHog/Trufflehog_python_script.py
@@ -14,7 +14,7 @@ repo_dir = '/var/lib/jenkins/workspace/Trufflehog_Job'
 # stdout=subprocess.PIPE argument redirects the output of the command to a pipe
 # stderr=subprocess.PIPE argument redirects the error of the command to a pipe
 # communicate() method waits for the command to complete and returns a tuple of (stdout, stderr) 
-output = subprocess.Popen(['trufflehog /var/lib/jenkins/workspace/Trufflehog_Job', '/var/lib/jenkins/workspace/Trufflehog_Job'], shell=True, stdout=subprocess.PIPE).communicate()[0]
+output = subprocess.Popen(['trufflehog3 /var/lib/jenkins/workspace/Trufflehog_Job', '/var/lib/jenkins/workspace/Trufflehog_Job'], shell=True, stdout=subprocess.PIPE).communicate()[0]
 
 # Print the output
 print(output.decode('utf-8'))
