3c7098a2be1c31552f9e1922e285e37d55d77d19
AWS-Jenkins <124871418+Spiderinattack@users.noreply.github.com>
2023-04-20 16:19:28 +0530

    Update Trufflehog_python_script.py

--- TruffleHog/Trufflehog_python_script.py
+++ TruffleHog/Trufflehog_python_script.py
@@ -6,7 +6,7 @@ import trufflehog3
 #install_trufflehog = subprocess.run('pip install trufflehog3', shell=True, capture_output=True, text=True)
 #print(install_trufflehog)
 
-output_txt = subprocess.call(["pip install trufflehog3"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output_txt = subprocess.call(["pip install trufflehog3"], shell=True ,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 print(output_txt.stdout.decode('utf-8'))
 
 # Specify the repository URL and directory to clone it to
