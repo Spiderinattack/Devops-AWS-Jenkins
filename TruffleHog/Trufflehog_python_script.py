c372ad8eef1910b379800c9a9517883f5394bd07
AWS-Jenkins <124871418+Spiderinattack@users.noreply.github.com>
2023-04-20 16:10:06 +0530

    Update Trufflehog_python_script.py

--- TruffleHog/Trufflehog_python_script.py
+++ TruffleHog/Trufflehog_python_script.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 import trufflehog3
 
-install_trufflehog=subprocess.call("pip install trufflehog3",shell=true,capture_output=True, text=True)
+install_trufflehog=subprocess.call("pip install trufflehog3",shell=true)
 #install_trufflehog = subprocess.run('pip install trufflehog3', shell=True, capture_output=True, text=True)
 print(install_trufflehog)
       
