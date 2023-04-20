b0fcdf88085173b88e893e4097273d9b6ac5dd90
AWS-Jenkins <124871418+Spiderinattack@users.noreply.github.com>
2023-04-20 16:24:52 +0530

    Update Trufflehog_python_script.py

--- TruffleHog/Trufflehog_python_script.py
+++ TruffleHog/Trufflehog_python_script.py
@@ -6,14 +6,14 @@ import trufflehog3
 #install_trufflehog = subprocess.run('pip install trufflehog3', shell=True, capture_output=True, text=True)
 #print(install_trufflehog)
 
-output_txt = subprocess.Popen(["pip install trufflehog3"], shell=True ,stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
+output_txt = subprocess.Popen(["pip3 install trufflehog"], shell=True ,stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
 print(output_txt.decode('utf-8'))
 
 # Specify the repository URL and directory to clone it to
 repo_url = 'https://github.com/Spiderinattack/Devops-AWS-Jenkins.git'
 repo_dir = '/var/lib/jenkins/workspace/Trufflehog_Job'
 
-trufflehog1 = subprocess.run('/home/ec2-user/.local/bin/trufflehog3  https://github.com/Spiderinattack/Devops-AWS-Jenkins.git', shell=True, capture_output=True, text=True)
+trufflehog1 = subprocess.run('/home/ec2-user/.local/bin/trufflehog  https://github.com/Spiderinattack/Devops-AWS-Jenkins.git', shell=True, capture_output=True, text=True)
 print(trufflehog1.stdout)
 print("Hello")
 
@@ -25,10 +25,10 @@ permission_check = subprocess.run('ls -l /var/lib/jenkins/workspace/TruffleHog_J
 print(permission_check.stdout)
 print("Hello")
 
-output = subprocess.Popen(['trufflehog /var/lib/jenkins/workspace/Trufflehog_Job', '/var/lib/jenkins/workspace/Trufflehog_Job'], shell=True, stdout=subprocess.PIPE).communicate()[0]
+#output = subprocess.Popen(['trufflehog /var/lib/jenkins/workspace/Trufflehog_Job', '/var/lib/jenkins/workspace/Trufflehog_Job'], shell=True, stdout=subprocess.PIPE).communicate()[0]
 
 # Print the output
-print(output.decode())
+#print(output.decode())
 
 #result1 = subprocess.run(['ls -l $(which trufflehog3)', '/var/lib/jenkins/workspace/Trufflehog_Job'], capture_output=True, text=True)
 #print(result1.stdout)
