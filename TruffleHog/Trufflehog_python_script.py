491d48f53d77efd0b5e5cd486106aea46bebfd7e
AWS-Jenkins <124871418+Spiderinattack@users.noreply.github.com>
2023-04-20 18:20:37 +0530

    Update Trufflehog_python_script.py

--- TruffleHog/Trufflehog_python_script.py
+++ TruffleHog/Trufflehog_python_script.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 
-os.environ['PATH'] +=  ':/home/ec2-user/.local/bin/'
+#os.environ['PATH'] +=  ':/home/ec2-user/.local/bin/'
 
 with open('output.txt' , 'w') as outfile:
     output_txt = subprocess.Popen(["/home/ec2-user/.local/bin/trufflehog https://github.com/Spiderinattack/Devops-AWS-Jenkins.git"], shell=True ,stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
