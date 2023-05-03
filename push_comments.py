9ce404909a8bf93de288b0d6b5f1b163c739a429
AWS-Jenkins <124871418+Spiderinattack@users.noreply.github.com>
2023-05-03 11:58:02 +0000

    Update push_comments.py

--- push_comments.py
+++ push_comments.py
@@ -1,35 +1,38 @@
 import requests
-from github import Github
-import subprocess
+import json
 
-access_token_git = "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"
+owner = 'Spiderinattack'
+repo = 'Devops-AWS-Jenkins'
+git_username = "Spiderinattack"
+git_api_token = "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"
 
-g = Github(access_token_git)
-repo = g.get_repo("Spiderinattack/Devops-AWS-Jenkins")
-pulls = repo.get_pulls(state='open')
-
-for pr in pulls:
-    pr_title = pr.title
-    pr_number = pr.number
-    print(pr_title)
-    print(pr_number)
-
-username = 'Spiderinattack'
-auth = (username, access_token_git)
+git_api_endpoint = f'https://api.github.com/repos/{owner}/{repo}/pulls'
+git_auth = (git_username, git_api_token)
 git_headers = {
     "Accept": "application/vnd.github.v3+json"
 }
 
-owner = 'Spiderinattack'
-repo = 'Devops-AWS-Jenkins'
-comments_api_endpoint = f'https://api.github.com/repos/{owner}/{repo}/issues/{pr_number}/comments'
+git_response = requests.get(git_api_endpoint, auth=git_auth, headers=git_headers)
 
+if git_response.status_code == 200:
+    pulls = json.loads(git_response.text)
+    if pulls and len(pulls) > 0:
+        for pr in pulls:
+            pr_number = pr['number']
+            pr_title = pr['title']
+            print(pr_number)
+            print(pr_title)
+    else:
+        print("There are no open PRs")
+else:
+    print(f"Failed to retrieve pull requests: {git_response.text}")
+    
+comments_api_endpoint = f'https://api.github.com/repos/{owner}/{repo}/issues/{pr_number}/comments'
 sed_command_pylint = "sed -n 's/.*Your code has been rated at \\([0-9]*\\.[0-9][0-9]\\).*/\\1/p' results/pylint_result.txt"
 score = subprocess.check_output(sed_command_pylint, shell=True, text=True, encoding='utf-8')
 print("Pylint score is", score)
 comment_body = 'Pylint score is ' + score
-
-response = requests.post(comments_api_endpoint, auth=auth, json={'body': comment_body})
+response = requests.post(comments_api_endpoint, auth=git_auth, json={'body': comment_body})
 print(response)
 
 if response.status_code == 201:
