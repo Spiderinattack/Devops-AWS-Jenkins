5eec94bc2dc7387a6fd7303f6138ff028a9fd852
AWS-Jenkins <124871418+Spiderinattack@users.noreply.github.com>
2023-05-04 08:53:06 +0000

    Update push_comments2.py

--- push_comments2.py
+++ push_comments2.py
@@ -1,7 +1,7 @@
 import requests
 from github import Github
 
-access_token_git = "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"
+access_token_git = "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"
 
 g = Github(access_token_git)
 repo = g.get_repo("Spiderinattack/Devops-AWS-Jenkins")
